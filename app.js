ac8f120ffb80d2ff2f2168a438e4daec261d75be
muhammadmehdi09 <120133578+muhammadmehdi09@users.noreply.github.com>
2024-02-08 13:21:38 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,61 +1,48 @@
-const person = document.getElementById("member")
+const name = document.getElementById("name1")
 const image = document.getElementById("img")
-const container = document.getElementById("container")
+const cards = document.getElementById("cards")
 
 image.addEventListener("change", () => {
     let reader = new FileReader();
     reader.readAsDataURL(image.files[0]);
     reader.addEventListener("load", () => {
-      image2 = `<img src=${reader.result} alt=''/>`;
+        image2 = `<img src=${reader.result} alt=''/>`;
     });
-  });
+});
 
-const membersData = []
+const people = []
 
-
-
-
-const loopHandler = () => {
-    container.innerHTML = ""
-    let i = 0
-    for (let item of membersData) {
-        container.innerHTML +=
+function dataExpresser() {
+    cards.innerHTML = ""
+    let index = 0
+    for (let item of people) {
+        cards.innerHTML +=
             `
             <div class="card bg-white 2xl:w-80 w-72 py-10 rounded-xl hover:scale-125 hover:rounded-2xl duration-500 h-[22rem] mb-16 m-auto">
-            <div class="w-32 mx-auto mb-3" id="image">${item.image}</div>
+            <div class="w-32 mx-auto mb-3">${item.image}</div>
             <p class="text-center">${item.name}</p>
-            <div class="my-3 text-center">
-                <span>${item.vote}</span>
-                <span>VOTES</span>
-            </div>
-            <button onclick="voteHandler(${i})" class="bg-green-800 px-10 py-3 text-white font-semibold text-2xl hover:text-green-700 hover:bg-black duration-200 rounded-xl 2xl:ml-[97px] ml-[81px]">Vote</button>
-            </br></br>
+            <div class="my-3 text-center">${item.vote} VOTES</div>
+            <button onclick="voteIncreaser(${index})" class="bg-green-800 px-10 py-3 text-white font-semibold text-2xl hover:text-green-700 hover:bg-black duration-200 rounded-xl 2xl:ml-[97px] ml-[81px]">Vote</button>
             </div>
              `
-        i = i + 1
+        index = index + 1
     }
 }
 
-const voteHandler = (index) => {
-    console.log(index);
-    console.log(membersData[index]);
-    membersData[index].vote += 1
-    loopHandler()
+function voteIncreaser(index) {
+    people[index].vote += 1
+    dataExpresser()
 }
 
-
-const submitHandler = () => {
-    const member = {
-        name: person.value,
+function formSubmiter() {
+    const data = {
+        name: name.value,
         image: image2,
         vote: 0
     }
-    membersData.push(member)
 
-    console.log(person.value);
-    console.log(membersData);
-    person.value = ""
-    loopHandler()
+    people.push(data)
+    dataExpresser()
 }
-loopHandler()
+dataExpresser()
 
